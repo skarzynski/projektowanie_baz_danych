620b3dc7350795f9c3ad9273c9e9ae1260aa1907
Jakub Kurczak <kubakurczak97@gmail.com>
2020-11-09 11:52:01 +0100

    add insert operations for: modifiers, accounts, branches, movies, genres, restrictions, genre_movie. Add few functions for generating data

--- GenerateData.py
+++ GenerateData.py
@@ -1,25 +1,28 @@
 import random
 from faker import Faker
+from db_connection import cursor
+import csv
+
 class GenerateData:
     # fake = Faker('pl_PL')
     fake = Faker('')
-    def GenerateIntegers(self, amountOfData, min = 0, max = 9999):
+
+    def GenerateIntegers(self, amountOfData, min=0, max=9999):
         listOfInteger = []
         for x in range(amountOfData):
-            listOfInteger.append(random.randint(min,max))
+            listOfInteger.append(random.randint(min, max))
         return listOfInteger
 
-    def GeneratePrices(self, amountOfData, min = 0.0, max = 9999.99):
+    def GeneratePrices(self, amountOfData, min=0.0, max=9999.99):
         listOfPrices = []
         for x in range(amountOfData):
-            listOfPrices.append(random.uniform(min,max))
+            listOfPrices.append(random.uniform(min, max))
         return listOfPrices
-
 
     def GenerateBooleans(self, amountOfData):
         listOfBolleans = []
         for x in range(amountOfData):
-            listOfInteger.append(random.randint(0,1))
+            listOfBolleans.append(random.randint(0, 1))
         return listOfBolleans
 
     def GenerateCities(self, amountOfData):
@@ -61,7 +64,8 @@ class GenerateData:
     def GeneratePasswords(self, amountOfData):
         listOfPasswords = []
         for x in range(amountOfData):
-            listOfPasswords.append(self.fake.password(random.randint(8,56), self.fake.boolean(), True, self.fake.boolean(), True))
+            listOfPasswords.append(
+                self.fake.password(random.randint(8, 56), self.fake.boolean(), True, self.fake.boolean(), True))
         return listOfPasswords
 
     def GenerateLogins(self, amountOfData):
@@ -76,7 +80,53 @@ class GenerateData:
             listOfEmails.add(self.fake.free_email())
         return listOfEmails
 
+        def GenerateForeignKeys(self, table_name):
+            sql = "SELECT id FROM " + table_name
+            cursor.execute(sql)
+            records = cursor.fetchall()
+            id_list = []
+            for string_val in records:
+                id_list.append(int(string_val[0]))
+            id_list.sort()
+            return id_list
+
+        def GenerateNotUniqueForeignKeysList(self, table_name, quantity):
+            foreign_keys = self.GenerateForeignKeys(table_name)
+            not_unique_foreign_keys_list = []
+            while len(not_unique_foreign_keys_list) < quantity:
+                not_unique_foreign_keys_list.append(foreign_keys[random.randint(0, len(foreign_keys) - 1)])
+            return not_unique_foreign_keys_list
+
+        def GenerateUniqueForeignKeysList(self, table_name, quantity):
+            foreign_keys = self.GenerateForeignKeys(table_name)
+            if len(foreign_keys) <= quantity:
+                return set(foreign_keys)
+            unique_foreign_keys = set()
+            while len(unique_foreign_keys) < quantity:
+                random_index = random.randint(0, len(foreign_keys) - 1)
+                unique_foreign_keys.add(foreign_keys[random_index])
+                foreign_keys.remove(foreign_keys[random_index])
+            return unique_foreign_keys
+
+        # its not random
+        def GenerateMovies(self, quantity):
+            movie_list = []
+            with open('movies.csv') as movie_file:
+                csv_reader = csv.reader(movie_file)
+                for row, _ in zip(csv_reader, range(0, quantity)):
+                    movie_list.append(row[1])
+            return movie_list
+
+        # risk of infinite loop
+        def GenerateUniqueGenreMovieList(self, quantity):
+
+            genre_movie = set()
+            while len(genre_movie) < quantity:
+                not_unique_movie_id_list = self.GenerateNotUniqueForeignKeysList("movies", quantity - len(genre_movie))
+                not_unique_genre_id_list = self.GenerateNotUniqueForeignKeysList("genres", quantity - len(genre_movie))
+                for movie_id, genre_id in zip(not_unique_movie_id_list, not_unique_genre_id_list):
+                    genre_movie.add((movie_id, genre_id))
+            return genre_movie
 
 if __name__ == "__main__":
     generate = GenerateData()
-    print(generate.GenerateEmails(100))
---
+++ insert_ops.py
@@ -0,0 +1,120 @@
+from db_connection import connection, cursor
+from GenerateData import GenerateData
+
+data_generator = GenerateData()
+
+
+def get_last_id(table_name):
+    id_list = data_generator.GenerateForeignKeys(table_name)
+    if len(id_list) > 0:
+        return id_list[len(id_list) - 1]
+    else:
+        return 0
+
+
+def insert_branch(city, address):
+    sql = "INSERT INTO branches(city, address) VALUES(%s,%s)"
+    values = (city, address)
+    cursor.execute(sql, values)
+    connection.commit()
+
+
+def insert_branches(quantity):
+    city_list = data_generator.GenerateCities(quantity)
+    address_list = data_generator.GenerateAddresses(quantity)
+    for index in range(0, quantity):
+        insert_branch(city_list[index], address_list[index])
+
+
+def insert_account(login, password, email):
+    sql = "INSERT INTO accounts(login, password,email) VALUES(%s,%s,%s)"
+    values = (login, password, email)
+    cursor.execute(sql, values)
+    connection.commit()
+
+
+def insert_accounts(quantity):
+    login_list = data_generator.GenerateLogins(quantity)
+    password_list = data_generator.GeneratePasswords(quantity)
+    email_list = data_generator.GenerateEmails(quantity)
+    for l, p, e in zip(login_list, password_list, email_list):
+        insert_account(l, p, e)
+
+
+def insert_modifier(type, name, value):
+    sql = "INSERT INTO modifiers(type, name, value) VALUES(%s,%s,%s)"
+    values = (str(type), name, str(value))
+    cursor.execute(sql, values)
+    connection.commit()
+
+
+# needs attention
+# modifiers name is prefix 'mod_' with id number of given modifier
+# value is 1% of randomly generated price
+# might be changed according to type of modifier
+def insert_modifiers(quantity):
+    type_list = data_generator.GenerateBooleans(quantity)
+    last_mod_id = get_last_id("modifiers")
+    value_list = data_generator.GeneratePrices(quantity)
+    for t, v in zip(type_list, value_list):
+        last_mod_id += 1
+        insert_modifier(t, "mod_" + str(last_mod_id), v * 0.01)
+
+
+def insert_restriction(rest_name):
+    sql = "INSERT INTO restrictions(name) VALUE(%s)"
+    values = (rest_name,)
+    cursor.execute(sql, values)
+    connection.commit()
+
+
+# restriction names source: https://en.wikipedia.org/wiki/Motion_picture_content_rating_system
+def insert_restrictions():
+    rest_list = ["G", "PG", "M", "R13", "R15", "R16", "R18", "RP13", "RP15", "RP18", "R"]
+    for rest in rest_list:
+        insert_restriction(rest)
+
+
+def insert_genre(genre_name):
+    sql = "INSERT INTO genres(name) VALUE(%s)"
+    values = (genre_name,)
+    cursor.execute(sql, values)
+    connection.commit()
+
+
+# data from http://files.grouplens.org/datasets/movielens/ml-latest-small-README.html
+def insert_genres():
+    genre_list = ["Action", "Adventure", "Animation", "Children's", "Comedy", "Crime", "Documentary", "Drama",
+                  "Fantasy", "Film-Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War",
+                  "Western"]
+    for genre in genre_list:
+        insert_genre(genre)
+
+
+def insert_movie(title, description, rest_id):
+    sql = "INSERT INTO movies(title, description, restriction_id) VALUES(%s,%s,%s)"
+    values = (title, description, str(rest_id))
+    cursor.execute(sql, values)
+    connection.commit()
+
+
+# for now without description
+def insert_movies(quantity):
+    title_list = data_generator.GenerateMovies(quantity)
+    description_list = []
+    rest_id_list = data_generator.GenerateNotUniqueForeignKeysList("restrictions", quantity)
+    for t, r_id in zip(title_list, rest_id_list):
+        insert_movie(t, "", r_id)
+
+
+def insert_genre_movie(movie_id, genre_id):
+    sql = "INSERT INTO genre_movie(movie_id, genre_id) VALUES(%s,%s)"
+    values = (str(movie_id), str(genre_id))
+    cursor.execute(sql, values)
+    connection.commit()
+
+
+def insert_genre_movies(quantity):
+    genre_movie_list = data_generator.GenerateUniqueGenreMovieList(quantity)
+    for genre_movie in genre_movie_list:
+        insert_genre_movie(genre_movie[0], genre_movie[1])
